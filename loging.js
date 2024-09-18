892f6ef7298786df92015990803bb872fc1165fb
ovisack <royavishek306@gmail.com>
2024-09-19 02:36:18 +0800

    first commit

--- loging.js
+++ loging.js
@@ -1,8 +1,37 @@
-document.getElementById('button-loging').addEventListener('click', function(event){
+// document.getElementById('button-loging').addEventListener('click', function(event){
     
-  event.preventDefault  
-    console.log('loing butn klick');
-    //stp-3: get the phon number
-  const phonNumber= document.getElementById('phon-number').value;
-  console.log(phonNumber);
+//   event.preventDefault  
+   
+//     //stp-3: get the phon number
+//   const phonNumber= document.getElementById('phon-number').value;
+
+//    const pinNumber= document.getElementById('password-enter').value;
+  
+//   console.log(phonNumber,pinNumber);
+  // this is tempuyari. you shold  do lik this 
+// if(phonNumber === '5' && pinNumber==='1234'){
+//     console.log('your are logd in ');
+//     //step -5 : allow user to us the website
+
+// }
+// else{
+//     alert('wrong phon number or pin ' )
+// }
+
+document.getElementById('button-loging')
+addEventListener('click', function(event){
+   event.preventDefault();
+   //get phon Number and pin 
+   const phonNumber= this.document.getElementById('phon-number').value;  
+   const pinNumber= document.getElementById('password-enter').value;
+   console.log(phonNumber,pinNumber);
+   if(phonNumber === '5' && pinNumber ==='1234'){
+    console.log('cart pin and number ');
+    window.location.href= '/home.html';
+    //step -5 : allow user to us the website
+
+}
+else{
+    alert('wrong phon number or pin ' )
+}
 })
